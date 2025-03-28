fed47044cd4262b138cd552a32721cfb11e82909
Hanno <nodscher@icloud.com>
2025-03-28 16:43:23 +0100

    improve laucnher.sh

--- packaging/launcher.sh
+++ packaging/launcher.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-trap 'trap - SIGTERM; kill %1 %2; pkill PeacockPatcher.' SIGINT SIGTERM EXIT
+trap 'trap - SIGTERM; kill %1 %2' SIGINT SIGTERM EXIT
 pkill PeacockPatcher.
 
 cd $(dirname "$0")
 
+nodepath=node
+[ -f "$HOME"/.nvm/versions/node/$(<.nvmrc)/bin/node ] && nodepath="$HOME"/.nvm/versions/node/"$(<.nvmrc)"/bin/node
+
+PORT=3500 $nodepath --enable-source-maps --harmony ./chunk0.js --hmr &
 ./PeacockPatcher.CLI &
-PORT=3500 "$HOME"/.nvm/versions/node/v23.9.0/bin/node --enable-source-maps --harmony ./chunk0.js --hmr &
 
 "$@"
